c03f3ce63931a76de542a9c38cceb7a82b098289
Zeed <Phiraphat.zim@mail.kmutt.ac.th>
2021-01-11 11:55:47 +0700

    Create BookInfor.xaml.cs

---
+++ BetaBookStoreApp/BetaBookStoreApp/BookInfor.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BetaBookStoreApp
+{
+    /// <summary>
+    /// Interaction logic for BookInformation.xaml
+    /// </summary>
+    public partial class BookInformation : Window
+    {
+        public BookInformation()
+        {
+            InitializeComponent();
+        }
+    }
+}
