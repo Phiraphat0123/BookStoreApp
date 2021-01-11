ded99e36ad97e7e3cc4b97b382d619217b93e7bf
Zeed <Phiraphat.zim@mail.kmutt.ac.th>
2021-01-11 11:55:32 +0700

    Update MainWindow.xaml.cs

--- BetaBookStoreApp/BetaBookStoreApp/MainWindow.xaml.cs
+++ BetaBookStoreApp/BetaBookStoreApp/MainWindow.xaml.cs
@@ -22,11 +22,14 @@ namespace BetaBookStoreApp
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {   
+    {   private string EmID;
+        private string Pw;
+        Dashboard dash;
         public MainWindow()
         {
             InitializeComponent();
             Data.CreatEmployeeTable();
+            dash = new Dashboard();
 
 
         }
@@ -40,10 +43,45 @@ namespace BetaBookStoreApp
         }
 
         private void BSignIn_Click(object sender, RoutedEventArgs e)
-        {
+        { EmID = txtID.Text;
+            Pw = txtPw.Password;
+            Boolean checkId= false;
+            Boolean checkPw = false;
+            foreach (string dataId in Data.GetEmployee("SELECT ID FROM EmployeeTable ")) 
+            {
+                if (EmID == dataId) {
+                    checkId = true;
+                }
+                
+            }
+            foreach (string dataPw in Data.GetEmployee("SELECT Password FROM EmployeeTable"))
+            {
+                if (Pw == dataPw) 
+                {
+                    checkPw = true;
+                }
+               
+            }
+            if (EmID == "" || Pw == "")
+            {
+                MessageBox.Show("Please enter information.", "ERROR");
+            }
+            else
+            if (checkId == true && checkPw == true)
+            {
+                dash.Show();
+                this.Close();
+                EmID = "";
+                Pw = "";
+            }
+            else 
+            {
+                MessageBox.Show("The ID or Password is incorrect, please try again.", "ERROR");
+            
+            }
 
         }
-        
-        
+
+       
     }
 }
