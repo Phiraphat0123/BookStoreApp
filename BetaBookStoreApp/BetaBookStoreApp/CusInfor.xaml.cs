7d661932862f7116d3845cecb09e602754c3b3c4
Zeed <Phiraphat.zim@mail.kmutt.ac.th>
2021-01-12 15:17:30 +0700

    Create CusInfor.xaml.cs

---
+++ BetaBookStoreApp/BetaBookStoreApp/CusInfor.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BetaBookStoreApp
+{
+    /// <summary>
+    /// Interaction logic for CusInfor.xaml
+    /// </summary>
+    public partial class CusInfor : Window
+    {
+        public CusInfor()
+        {
+            InitializeComponent();
+        }
+
+        private void AddCus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditCus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShowAllCus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteCus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
