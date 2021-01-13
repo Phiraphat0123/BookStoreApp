59edbb2b941fb396ce66a8cab1c7c4aac035edcf
Zeed <Phiraphat.zim@mail.kmutt.ac.th>
2021-01-13 17:49:31 +0700

    Create OrderReport.xaml.cs

---
+++ BetaBookStoreApp/BetaBookStoreApp/OrderReport.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BetaBookStoreApp
+{
+    /// <summary>
+    /// Interaction logic for OrderReport.xaml
+    /// </summary>
+    public partial class OrderReport : Window
+    {
+        public OrderReport()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            Dashboard dash = new Dashboard();
+            dash.Show();
+            this.Close();
+        }
+    }
+}
